7965232ef51c1e7eacd028674510dfac6fc46747
Mark <mark.oconnor14@gmail.com>
2024-11-28 09:37:24 +0800

    make changes to reflect shown answers

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 
 func main() {
+	const port = "8080"
+
 	mux := http.NewServeMux()
-	server := http.Server{
-		Addr: ":8080",
+
+	server := &http.Server{
+		Addr: ":" + port,
 		Handler: mux,
 	}
-	server.ListenAndServe()
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
 }
