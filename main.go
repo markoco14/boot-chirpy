e798d79d73cd40a0d78cf44368b146455cd21991
Mark <mark.oconnor14@gmail.com>
2024-11-30 12:04:52 +0800

    update code to reflect solutions

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 	"sync/atomic"
-	"encoding/json"
 )
 
 type apiConfig struct {
@@ -20,55 +19,20 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	
-	mux.HandleFunc("/api/validate_chirp", func(w http.ResponseWriter, r *http.Request){
-		type parameters struct {
-			Body string `json:"body"`
-		}
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
 
-		type responseBody struct {
-			Valid bool `json:"valid"`
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		decoder := json.NewDecoder(r.Body)
-		params := parameters{}
-		err := decoder.Decode(&params)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(`{"error": "Something went wrong"}`))
-			return
-		}
-		
-		if len(params.Body) > 140 {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(`{"error": "Chirp is too long"}`))
-			return
-		}
-		resp := responseBody{Valid: true}
-		data, err := json.Marshal(resp)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(`{"error": "Something went wrong"}`))
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write(data)
-	})
-	
-	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 
 	srv := &http.Server{
-		Addr: ":" + port,
+		Addr:    ":" + port,
 		Handler: mux,
 	}
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
-
--- metrics.go
+++ metrics.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 )
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf(`
-		<html>
-		  <body>
-			<h1>Welcome, Chirpy Admin</h1>
-			<p>Chirpy has been visited %d times!</p>
-		  </body>
-		</html>`, cfg.fileserverHits.Load())))
+<html>
+
+<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %dtimes!</p>
+</body>
+
+</html>
+	`, cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
