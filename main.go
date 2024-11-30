71a6442477e9835d0f1a7b0a57089c6c7a1031ee
Mark <mark.oconnor14@gmail.com>
2024-11-30 12:00:47 +0800

    complete ch4 json lesson

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"sync/atomic"
+	"encoding/json"
 )
 
 type apiConfig struct {
@@ -23,9 +24,45 @@ func main() {
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	
+	mux.HandleFunc("/api/validate_chirp", func(w http.ResponseWriter, r *http.Request){
+		type parameters struct {
+			Body string `json:"body"`
+		}
+
+		type responseBody struct {
+			Valid bool `json:"valid"`
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		decoder := json.NewDecoder(r.Body)
+		params := parameters{}
+		err := decoder.Decode(&params)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(`{"error": "Something went wrong"}`))
+			return
+		}
+		
+		if len(params.Body) > 140 {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(`{"error": "Chirp is too long"}`))
+			return
+		}
+		resp := responseBody{Valid: true}
+		data, err := json.Marshal(resp)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"error": "Something went wrong"}`))
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(data)
+	})
+	
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	
+
 	srv := &http.Server{
 		Addr: ":" + port,
 		Handler: mux,
