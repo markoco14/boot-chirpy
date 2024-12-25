c5c657d9f89159e70077e9f61641b38d50acb6ed
Mark <mark.oconnor14@gmail.com>
2024-12-25 12:06:52 +0800

    do migrations step from authentication part 1

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,7 +2,8 @@ CREATE TABLE users (
   id UUID PRIMARY KEY,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
-  email TEXT NOT NULL UNIQUE
+  email TEXT NOT NULL UNIQUE,
+  hashed_password TEXT NOT NULL DEFAULT 'unset'
 );
 
 CREATE TABLE chirps (
