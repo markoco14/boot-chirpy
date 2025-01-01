e968339536c464e0e6327c922fc11034b6a7140b
Mark <mark.oconnor14@gmail.com>
2025-01-01 22:29:42 +0800

    add refresh_tokens table to database

--- sql/schema.sql
+++ sql/schema.sql
@@ -13,3 +13,12 @@ CREATE TABLE chirps (
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     body TEXT NOT NULL
 );
+
+CREATE TABLE refresh_tokens (
+    token text PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    expires_at TIMESTAMP NOT NULL,
+    revoked_at TIMESTAMP
+);
